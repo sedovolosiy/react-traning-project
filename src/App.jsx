ef333d9818db74f79007428497dd743b97530c1a
Serhii Siedovolosyi <sedovolosiy@gmail.com>
2025-05-26 09:46:36 +0200

    Refactor Header component to use destructuring for props and add Main component to display furniture list

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
 import "./App.css";
 
-function Header(props) {
+// вместо props.name и props.year используем деструктуризацию
+// props - это объект, содержащий все переданные свойства
+// в компонент Header, поэтому мы можем использовать деструктуризацию
+// для извлечения нужных свойств напрямую в параметры функции
+// props.name и props.year можно заменить на name и year
+function Header({ name, year }) {
   return (
     <header>
-      <h1>{props.name}'s Room</h1>
-      <p>Copyright: {props.year}</p>
+      <h1>{name}'s Room</h1>
+      <p>Copyright: {year}</p>
     </header>
+  );
+}
+
+const furniture = [
+  { id: 1, name: "Chair", price: 50 },
+  { id: 2, name: "Table", price: 150 },
+  { id: 3, name: "Sofa", price: 300 },
+];
+
+function Main({ furniture }) {
+  return (
+    <main>
+      <h2>Furniture List</h2>
+      <ul>
+        {furniture.map((item) => (
+          <li style={{ listStyleType: "none" }} key={item.id}>
+            {item.name} - ${item.price}
+          </li>
+        ))}
+      </ul>
+    </main>
   );
 }
 
@@ -17,6 +43,7 @@ function App() {
         <h2>Welcome to my room!</h2>
         <p>This is a simple React application.</p>
       </main>
+      <Main furniture={furniture} />
     </div>
   );
 }
